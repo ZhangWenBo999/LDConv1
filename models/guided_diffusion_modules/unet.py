d33c4d7e7a88290f75caf439500475ed0260b272
ZhangWenBo999 <131420945+ZhangWenBo999@users.noreply.github.com>
2024-11-05 14:26:50 +0800

    Update unet.py

--- models/guided_diffusion_modules/unet.py
+++ models/guided_diffusion_modules/unet.py
@@ -189,8 +189,8 @@ class ResBlock(EmbedBlock):
             # )
             self.skip_connection = LDConv_DynamicSampling(inc=channels,outc=self.out_channel,num_param=3)
         else:
-            # self.skip_connection = nn.Conv2d(channels, self.out_channel, 1)
-            self.skip_connection = LDConv_DynamicSampling(inc=channels,outc=self.out_channel,num_param=3)
+            self.skip_connection = nn.Conv2d(channels, self.out_channel, 1)
+            # self.skip_connection = LDConv_DynamicSampling(inc=channels,outc=self.out_channel,num_param=3)
 
     def forward(self, x, emb):
         """
